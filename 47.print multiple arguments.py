738a7efe10a8bbd4c4febd31ac49745a0fdb3db9
Sivapriyan <sivapriyan7siva@gmail.com>
2022-06-24 15:58:06 +0530

    First commit

---
+++ 47.print multiple arguments.py
@@ -0,0 +1,5 @@
+def GFG(name, num):
+    print("Hello from ", name + ', ' + num)
+
+
+GFG("sivapriyan", "25")
---
+++ 49.sorting user defined class.py
@@ -0,0 +1,3 @@
+print(sorted([1,26,3,9]))
+
+print(sorted("sivapriyan in coder".split(), key=str.lower))
